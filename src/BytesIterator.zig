79d1c6e7bf048e665f69ccbc5a6df4231ca3ba35
andrewkreuzer <me@andrewkreuzer.com>
2025-06-01 19:53:01 -0400

    test: BytesIterator

--- src/BytesIterator.zig
+++ src/BytesIterator.zig
@@ -60,6 +60,13 @@ pub fn peek(self: *Self, T: TypeSignature, t: ?TypeSignature) ?struct{ []const u
             self.buffer[alignment..][0..@sizeOf(f64)],
             offset + @sizeOf(f64)
         },
+        .string, .object_path  => blk: {
+            const slice = self.buffer[alignment..][0..@sizeOf(u32)];
+            const len = mem.readInt(u32, slice, .little);
+            const ret = self.buffer[alignment+@sizeOf(u32)..][0..len];
+            const n = offset + len + @sizeOf(u32) + 1;
+            break :blk .{ ret, n }; // 1 for null byte
+        },
         .signature => blk: {
             const slice = self.buffer[alignment..][0..@sizeOf(u8)];
             const len = mem.readInt(u8, slice, .little);
@@ -67,31 +74,67 @@ pub fn peek(self: *Self, T: TypeSignature, t: ?TypeSignature) ?struct{ []const u
             const n = offset + len + @sizeOf(u8) + 1; // 1 for null byte
             break :blk .{ ret, n };
         },
-        .variant => blk: {
-            const slice = self.buffer[alignment..][0..@sizeOf(u8)];
-            const len = mem.readInt(u8, slice, .little);
-            const ret = self.buffer[alignment+@sizeOf(u8)..][0..len];
-            const n = offset + len + @sizeOf(u8) + 1; // 1 for null byte
-            break :blk .{ ret, n }; // 1 for null byte
-        },
-        .object_path, .string => blk: {
-            const slice = self.buffer[alignment..][0..@sizeOf(u32)];
-            const len = mem.readInt(u32, slice, .little);
-            const ret = self.buffer[alignment+@sizeOf(u32)..][0..len];
-            const n = offset + len + @sizeOf(u32) + 1;
-            break :blk .{ ret, n }; // 1 for null byte
-        },
         .array => blk: {
             const slice = self.buffer[alignment..][0..@sizeOf(u32)];
             const len = mem.readInt(u32, slice, .little);
             const offset_ = t.?.alignOffset(alignment + @sizeOf(u32));
             alignment += offset_;
             const ret = self.buffer[alignment+@sizeOf(u32)..][0..len];
-            const n = offset + offset_ + len + @sizeOf(u32) + 1;
+            const n = offset + offset_ + len + @sizeOf(u32);
             break :blk .{ ret, n };
         },
         .@"struct", .dict_entry  => .{ self.buffer[alignment..], offset },
+        .variant => blk: {
+            const slice = self.buffer[alignment..][0..@sizeOf(u8)];
+            const len = mem.readInt(u8, slice, .little);
+            const ret = self.buffer[alignment+@sizeOf(u8)..][0..len];
+            const n = offset + len + @sizeOf(u8) + 1; // 1 for null byte
+            break :blk .{ ret, n };
+        },
         else =>  null,
     };
 }
 
+test "peek" {
+    const tests = [_]struct{
+        name: []const u8,
+        buffer: []const u8,
+        index: usize,
+        T: TypeSignature,
+        t: ?TypeSignature = null,
+        expected: ?struct{ []const u8, usize },
+    }{
+        .{ .name = "byte", .buffer = "a", .index = 0, .T = .byte, .t = null, .expected = .{ "a", 1 } },
+        .{ .name = "boolean", .buffer = "\x01\x00\x00\x00", .index = 0, .T = .boolean, .t = null, .expected = .{ "\x01\x00\x00\x00", 4 } },
+        .{ .name = "int16", .buffer = "\x01\x00", .index = 0, .T = .int16, .t = null, .expected = .{ "\x01\x00", 2 } },
+        .{ .name = "uint16", .buffer = "\x01\x00", .index = 0, .T = .uint16, .t = null, .expected = .{ "\x01\x00", 2 } },
+        .{ .name = "int32", .buffer = "\x01\x00\x00\x00", .index = 0, .T = .int32, .t = null, .expected = .{ "\x01\x00\x00\x00", 4 } },
+        .{ .name = "uint32", .buffer = "\x01\x00\x00\x00", .index = 0, .T = .uint32, .t = null, .expected = .{ "\x01\x00\x00\x00", 4 } },
+        .{ .name = "int64", .buffer = "\x01\x00\x00\x00\x00\x00\x00\x00", .index = 0, .T = .int64, .t = null, .expected = .{ "\x01\x00\x00\x00\x00\x00\x00\x00", 8 } },
+        .{ .name = "uint64", .buffer = "\x01\x00\x00\x00\x00\x00\x00\x00", .index = 0, .T = .uint64, .t = null, .expected = .{ "\x01\x00\x00\x00\x00\x00\x00\x00", 8 } },
+        .{ .name = "double", .buffer = "\x01\x00\x00\x00\x00\x00\x00\x00", .index = 0, .T = .double, .t = null, .expected = .{ "\x01\x00\x00\x00\x00\x00\x00\x00", 8 } },
+        .{ .name = "string", .buffer = "\x04\x00\x00\x00test\x00", .index = 0, .T = .string, .t = null, .expected = .{ "test", 9 } },
+        .{ .name = "object_path", .buffer = "\x04\x00\x00\x00test\x00", .index = 0, .T = .object_path, .t = null, .expected = .{ "test", 9 } },
+        .{ .name = "signature", .buffer = "\x04test\x00", .index = 0, .T = .signature, .t = null, .expected = .{ "test", 6 } },
+        .{ .name = "array", .buffer = "\x09\x00\x00\x00\x04\x00\x00\x00test\x00", .index = 0, .T = .array, .t = .string, .expected = .{ "\x04\x00\x00\x00test\x00", 13 } },
+        .{ .name = "struct", .buffer = "\x07\x00\x00\x00\x08\x00\x00\x00\x09\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x04\x00\x00\x00\x01\x00\x02\x00", .index = 0, .T = .@"struct", .t = null, .expected = .{ "\x07\x00\x00\x00\x08\x00\x00\x00\x09\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x04\x00\x00\x00\x01\x00\x02\x00", 0 } },
+        .{ .name = "variant", .buffer = "\x01s\x00\x05\x00\x00\x00test\x00", .index = 0, .T = .variant, .t = null, .expected = .{ "s", 3 } },
+        .{ .name = "dict_entry", .buffer = "\x03\x01\x73\x00\x04\x00\x00\x00\x54\x65\x73\x74\x00", .index = 0, .T = .dict_entry, .t = null, .expected = .{ "\x03\x01\x73\x00\x04\x00\x00\x00\x54\x65\x73\x74\x00", 0 } },
+        .{ .name = "invalid", .buffer = "test", .index = 0, .T = .byte, .t = null, .expected = null },
+
+    };
+
+    for (tests) |t| {
+        var iter = Self{
+            .buffer = t.buffer,
+            .index = t.index,
+        };
+        const result = iter.peek(t.T, t.t);
+        if (t.expected) |expected| if (result) |r| {
+            try std.testing.expectEqualSlices(u8, expected.@"0", r.@"0");
+            try std.testing.expectEqual(expected.@"1", r.@"1");
+        } else {
+            try std.testing.expect(t.expected == null);
+        };
+    }
+}
